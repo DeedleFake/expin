8613f63d3693521d2e018e864880cffd25349ba7
DeedleFake <deedlefake@users.noreply.github.com>
2024-05-11 22:40:14 -0400

    add function skeletons for other pin endpoints

--- lib/expin_web/pin_controller.ex
+++ lib/expin_web/pin_controller.ex
@@ -2,6 +2,22 @@ defmodule ExpinWeb.PinController do
   use ExpinWeb, :controller
 
   def list(conn, _params) do
-    conn |> json(%{this: :is, a: :test})
+    conn |> json(%{not: :implemented})
+  end
+
+  def add(conn, _params) do
+    conn |> json(%{not: :implemented})
+  end
+
+  def get(conn, %{"request_id" => request_id}) do
+    conn |> json(%{not: :implemented, request_id: request_id})
+  end
+
+  def replace(conn, %{"request_id" => request_id}) do
+    conn |> json(%{not: :implemented, request_id: request_id})
+  end
+
+  def remove(conn, %{"request_id" => request_id}) do
+    conn |> json(%{not: :implemented, request_id: request_id})
   end
 end
