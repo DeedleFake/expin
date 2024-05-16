effa701bc811858bda9c961b49fbf2eff848fd60
DeedleFake <deedlefake@users.noreply.github.com>
2024-05-16 12:06:32 -0400

    fix API breakage

--- lib/expin/pins.ex
+++ lib/expin/pins.ex
@@ -24,17 +24,16 @@ defmodule Expin.Pins do
   Creates a pin.
   """
   def create_pin(attrs \\ %{}) do
-    %Pin{}
-    |> Pin.changeset(attrs)
+    Pin.create_changeset(attrs)
     |> Repo.insert()
   end
 
   @doc """
   Updates a pin.
   """
-  def update_pin(%Pin{} = pin, attrs) do
-    pin
-    |> Pin.changeset(attrs)
+  def update_pin_status(id, status) do
+    %Pin{id: id}
+    |> Pin.update_status_changeset(%{status: status})
     |> Repo.update()
   end
 
@@ -43,12 +42,5 @@ defmodule Expin.Pins do
   """
   def delete_pin(%Pin{} = pin) do
     Repo.delete(pin)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking pin changes.
-  """
-  def change_pin(%Pin{} = pin, attrs \\ %{}) do
-    Pin.changeset(pin, attrs)
   end
 end
